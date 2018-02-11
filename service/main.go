b51a7a670e17342bad1327b674dd5a2f1ca838ed
weijian2 <liweijian1023@outlook.com>
2018-02-11 14:49:48 -0500

    add search handler

--- service/main.go
+++ service/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"encoding/json"
 	"log"
+	"strconv"
 )
 
 // type/struct are keywords in GO, struct is similar to class in java, Location is struct name
@@ -20,10 +21,15 @@ type Post struct {
 	Location Location `json:"location"`
 }
 
+const (
+	DISTANCE = "200km"
+)
+
 func main() {
-	fmt.Println("started-service")
+	fmt.Println("service start")
 	http.HandleFunc("/post", handlerPost)
-	log.Fatal(http.ListenAndServe(":1212", nil))
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
@@ -36,4 +42,38 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
+
+// http://localhost:8888/search?lat=10.0&lon=20.0
+// http://localhost:8888/search?lat=10.0&lon=20.0&range=100
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}
+	fmt.Println("range is ", ran)
+
+	// return fake post
+	p := &Post {
+		User:"Weijian",
+		Message:"是中国人就转，不转不是人",
+		Location: Location {
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	js, err := json.Marshal(p)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+
+	//fmt.Fprintf(w, "Search received: %s %s", lat, lon)
 }
