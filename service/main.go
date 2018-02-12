937f08b4bcbca57a6a68d026d85b956aff800673
weijian2 <liweijian1023@outlook.com>
2018-02-11 20:28:56 -0500

    update handlerSearch function so that it can support range search in database

--- service/main.go
+++ service/main.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"log"
 	"strconv"
+	"reflect"
+	"gopkg.in/olivere/elastic.v3"
 )
 
 // type/struct are keywords in GO, struct is similar to class in java, Location is struct name
@@ -22,8 +24,16 @@ type Post struct {
 }
 
 const (
+	INDEX = "around"
+	TYPE = "post"
 	DISTANCE = "200km"
+	// Needs to update
+	//PROJECT_ID = "around-xxx"
+	//BT_INSTANCE = "around-post"
+	// Needs to update this URL if you deploy it to cloud.
+	ES_URL = "http://35.196.197.253:9200"
 )
+
 
 func main() {
 	fmt.Println("service start")
@@ -54,26 +64,83 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	if val := r.URL.Query().Get("range"); val != "" {
 		ran = val + "km"
 	}
-	fmt.Println("range is ", ran)
+	//fmt.Println("range is ", ran)
 
-	// return fake post
-	p := &Post {
-		User:"Weijian",
-		Message:"是中国人就转，不转不是人",
-		Location: Location {
-			Lat: lat,
-			Lon: lon,
-		},
+	fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
+
+	// Create a client，which means we create a connection to ES. If there is err, return.
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
 	}
 
-	js, err := json.Marshal(p)
+	// Define geo distance query as specified in
+	// https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
+	// Prepare a geo based query to find posts within a geo box.
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	// Some delay may range from seconds to minutes.
+	// Get the results based on Index (similar to dataset) and query (q that we just prepared).
+	// Pretty means to format the output.
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	// searchResult is of type SearchResult and returns hits, suggestions,
+	// and all kinds of other information from Elasticsearch.
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	// TotalHits is another convenience function that works even when something goes wrong.
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+	// Each is a convenience function that iterates over hits in a search result.
+	// It makes sure you don't need to check for nil values in the response.
+	// However, it ignores errors in serialization.
+	var typ Post
+	var ps []Post
+	// Iterate the result results and if they are type of Post (typ)
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		p := item.(Post) // Cast an item to Post, equals to p = (Post) item in java
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+		// TODO: Perform filtering based on keywords such as web spam etc.
+		ps = append(ps, p)
+
+	}
+	js, err := json.Marshal(ps) // Convert the go object to a string
 	if err != nil {
 		panic(err)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 
-	//fmt.Fprintf(w, "Search received: %s %s", lat, lon)
+	//// return fake post
+	//p := &Post {
+	//	User:"Weijian",
+	//	Message:"是中国人就转，不转不是人",
+	//	Location: Location {
+	//		Lat: lat,
+	//		Lon: lon,
+	//	},
+	//}
+	//
+	//js, err := json.Marshal(p)
+	//if err != nil {
+	//	panic(err)
+	//	return
+	//}
+	//
+	//w.Header().Set("Content-Type", "application/json")
+	//w.Write(js)
+	//
+	////fmt.Fprintf(w, "Search received: %s %s", lat, lon)
 }
