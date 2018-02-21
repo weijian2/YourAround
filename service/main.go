10f021b8edf53af47e9a8a239af6f549848d69f1
weijian2 <liweijian1023@outlook.com>
2018-02-21 13:24:30 -0500

    update handlerPost function to handle multipart form and store post with image to Google Cloud Storage

--- service/main.go
+++ service/main.go
@@ -10,6 +10,9 @@ import (
 	"gopkg.in/olivere/elastic.v3"
 	"github.com/pborman/uuid"
 	"strings"
+	"context"
+	"cloud.google.com/go/storage"
+	"io"
 )
 
 // type/struct are keywords in GO, struct is similar to class in java, Location is struct name
@@ -23,6 +26,7 @@ type Post struct {
 	User     string `json:"user"`
 	Message  string  `json:"message"`
 	Location Location `json:"location"`
+	Url    string `json:"url"`
 }
 
 const (
@@ -32,8 +36,8 @@ const (
 	// Needs to update
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
-	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.68.27:9200"
+	ES_URL = "http://35.202.253.25:9200/"
+	BUCKET_NAME = "post-images-youraround-cmu"
 )
 
 
@@ -80,22 +84,68 @@ func main() {
 	fmt.Println("service start")
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// how to parse multipart form in Go?
+// https://golang.org/pkg/net/http/#Request.ParseMultipartForm
+// https://github.com/golang-samples/http/blob/master/fileupload/main.go
 func handlerPost(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
+
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
 		return
 	}
-	id := uuid.New()
-	// Save user post to ElasticSearch.
-	saveToES(&p, id)
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	// defer here because if some codes break in the middle of method, file.close() will not be executed
+	defer file.Close()
+
+	// fetch authorized user from local config file, must first authorized with Google
+	ctx := context.Background()
+
+	// attrs is file
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+	// Save to ES.
+	saveToES(p, id)
+
+	// Save to BigTable.
+	//saveToBigTable(p, id)
+
 }
 
 // http://localhost:8888/search?lat=10.0&lon=20.0
@@ -203,4 +253,44 @@ func containsSensitiveWords(post *string) bool {
 		}
 	}
 	return false
+}
+
+// private method, save image to Google Cloud Storage
+// Google example of open a client connection to GCS
+// https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-go
+// Google example of writing an object to GCS, see write function
+// https://github.com/GoogleCloudPlatform/golang-samples/blob/master/storage/objects/main.go
+// r is image file, name is id of this image
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
+
+	// Creates a Bucket instance.
+	bucket := client.Bucket(bucketName)
+	// Next check if the bucket exists
+	if _, err = bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bucket.Object(name) // stored file
+	w := obj.NewWriter(ctx)
+	// copy(write) image file to GCS's bucket
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	// already finish writting, now modify access permission to all users, ACL is access control list
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
 }
