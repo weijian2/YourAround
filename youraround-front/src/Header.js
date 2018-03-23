0be0ecf722d7b8d0e2886f319589f7a3b4e88af0
weijian2 <liweijian1023@outlook.com>
2018-03-23 12:46:07 -0400

    Add Header.js file

---
+++ youraround-front/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+        );
+    }
+}
