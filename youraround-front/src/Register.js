c75041764c8201a11f105fb0c7d4bae62b9d03ee
weijian2 <liweijian1023@outlook.com>
2018-03-23 16:59:43 -0400

    add ajax call(register) to backend server

--- youraround-front/src/Register.js
+++ youraround-front/src/Register.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Form, Input, Checkbox, Button } from 'antd';
+import { Form, Input, Checkbox, Button, message } from 'antd';
+import $ from 'jquery'
+import { API_ROOT} from "./constants"
+
 const FormItem = Form.Item;
 
 
@@ -15,6 +18,20 @@ class RegistrationForm extends React.Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/signup`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password,
+                    })
+                }).then((response) => {
+                    message.success(response);
+                }, (response) => {
+                    message.error(response.responseText);
+                }).catch((error) => {
+                    console.log(error);
+                });
             }
         });
     }
