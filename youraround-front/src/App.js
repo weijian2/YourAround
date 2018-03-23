dae1fa69bfe68200e41afa39f344ee1d7ef6c248
weijian2 <liweijian1023@outlook.com>
2018-03-23 15:00:28 -0400

    Add Main.js

--- youraround-front/src/App.js
+++ youraround-front/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Header } from './Header'
-import { Header_index } from "./Header_index"
+import { Header_index } from './Header_index';
+import { Main } from './Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header_index/>
+        <Main/>
       </div>
     );
   }
---
+++ youraround-front/src/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div>this is main!</div>
+        );
+    }
+}
