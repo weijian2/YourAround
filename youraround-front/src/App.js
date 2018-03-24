7f94ff78de078a3aa4e7caa27893fb7c6ef5792f
weijian2 <liweijian1023@outlook.com>
2018-03-24 17:04:49 -0400

    Change some comments

--- youraround-front/src/App.js
+++ youraround-front/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header_index/>
+        <Header/>
         <Main/>
       </div>
     );
--- youraround-front/src/constants.js
+++ youraround-front/src/constants.js
@@ -1,1 +1,3 @@
-export const API_ROOT = 'https://around-75015.appspot.com';
+//export const API_ROOT = 'https://around-75015.appspot.com';
+//export const API_ROOT = 'http://localhost:8080';
+export const API_ROOT = 'https://youraround-cmu.appspot.com';
