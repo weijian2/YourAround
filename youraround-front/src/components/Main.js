051360d3dfa2cea3ba43c6eabe6d0835a8ecd58d
weijian2 <liweijian1023@outlook.com>
2018-03-24 17:27:20 -0400

    add Login.js

--- youraround-front/src/components/Main.js
+++ youraround-front/src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Register } from './Register'
+import { Login } from './Login'
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Register/>
+                <Login/>
             </div>
         );
     }
