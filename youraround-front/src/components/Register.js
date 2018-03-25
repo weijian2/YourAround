6e8c78a0d3a5f57ce1bfeeef88ce60b1a3d52745
weijian2 <liweijian1023@outlook.com>
2018-03-24 22:12:13 -0400

    add redirect to login page after register

--- youraround-front/src/components/Register.js
+++ youraround-front/src/components/Register.js
@@ -27,6 +27,7 @@ class RegistrationForm extends React.Component {
                     })
                 }).then((response) => {
                     message.success(response);
+                    this.props.history.push('login');
                 }, (response) => {
                     message.error(response.responseText);
                 }).catch((error) => {
